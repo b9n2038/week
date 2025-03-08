0aa40662cfd0407c82bbfa5ba228333f4be88a31
b9n2038 <b9n2038@icloud.com>
2025-03-09 07:53:35 +1000

    wip: parsing some year change dates not correc

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -34,7 +34,6 @@ func newOfCmd() *cobra.Command {
 		Use:   "of",
 		Short: "translate a date dd [MM] [yy[yyyy]] format into ISOWeek and day.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Printf("todo parse the dd mm yy\n")
 
 			today := time.Now()
 			day := int(today.Day())
@@ -52,24 +51,31 @@ func newOfCmd() *cobra.Command {
 				if len(args[2]) == 4 {
 					yr, _ = strconv.Atoi(args[2])
 				}
-				if len(args[2]) == 2 {
-					yr, _ = strconv.Atoi("20" + args[2])
+				if len(args[2]) <= 2 {
+					yr, _ = strconv.Atoi(args[2])
+					yr = 2000 + yr
 				}
 				//else silent use current yr
 			}
 
 			target := time.Date(yr, time.Month(mth), day, 0, 0, 0, 0, time.UTC)
 
-			_, iweek := isoweek.FromDate(target.Year(), target.Month(), target.Day())
-			startYr, startMth, _ := isoweek.StartDate(today.Year(), iweek)
+			//1 1 27 works, but 1 1 has 24 wk01 as yr, not 25w53 if thats what it is meant to be
+			// fmt.Printf("target yr %d, %d, %d\n", yr, mth, day)
+			fromYr, fromWeek := isoweek.FromDate(target.Year(), target.Month(), target.Day())
+			// fmt.Printf("fromYr %d, %d\n", fromYr, fromWeek)
+			//to get the weekday, need more conversion
+			startYr, startMth, _ := isoweek.StartDate(fromYr, fromWeek)
+			// fmt.Printf("start yr %d, %d \n", startYr, startMth)
 			wd := isoweek.ISOWeekday(startYr, startMth, day)
-			shortYr := target.Format("06") // Last two digits of year
+			// shortYr := startYr.Format("06") // Last two digits of year
+			// fmt.Printf("weekday %d,\n", wd)
 
 			//todo: check long flag format
 			if formatWithWeekday {
-				fmt.Printf("%sw%02d-%d\n", shortYr, iweek, wd)
+				fmt.Printf("%02dw%02d-%d\n", startYr%1000, fromWeek, wd)
 			} else {
-				fmt.Printf("%sw%02d\n", shortYr, iweek)
+				fmt.Printf("%02dw%02d\n", startYr%1000, fromWeek)
 			}
 			return nil
 		},
